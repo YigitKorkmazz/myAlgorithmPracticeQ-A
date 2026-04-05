3fc4b561b324d583a46632d72c9755d742b53484
YusufYigitKorkmaz <yigit.korkmaz@ug.bilkent.edu.tr>
2026-04-06 00:02:06 +0300

    bottom up DP solution for Longest increasing subsequence problem

---
+++ LIS.java
@@ -0,0 +1,30 @@
+public class LIS {
+    //Given an integer array nums, return the length of the longest strictly increasing subsequence.
+    //BOTTOM UP DP SOLUTION
+    public int lengthOfLIS(int[] nums) {
+        int n = nums.length;
+        int[] dp = new int[n];
+
+        for (int i = 0 ; i < n; i++){
+            dp[i] = 1;
+        }
+
+        for (int i = 1; i < n; i ++){
+            for (int j = 0; j < i; j++){
+                if (nums[i] > nums[j]){
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+        }
+
+        int max = Integer.MIN_VALUE;
+        for(int i = 0 ; i < dp.length; i++)
+        {
+            if (dp[i] > max){
+                max = dp[i];
+            }
+        }
+
+        return max;
+    }
+}
